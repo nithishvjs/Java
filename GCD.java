3309d4f74c4eac12826e831a2b18dc58aa6afaa2
Nithish Akash V <17nithishakash@gmail.com>
2026-04-17 16:00:34 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int gcd = 1;
+        for(int i = 2 ; i <= Math.min(a, b) ; i++){
+            if(a % i == 0 && b % i == 0)
+            gcd = i;
+        }
+        System.out.println(gcd);
+    }
+}
